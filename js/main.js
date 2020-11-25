4927a1879fcce7df3ae5f01150d194474aac248b
Slava Pogosov <pogoslav1@gmail.com>
2020-11-25 18:29:46 +0300

    create: update code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-import Swiper, {
-  Navigation,
-  Pagination
-} from "swiper";
-Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper(".hotel__slider", {
+// const burger = document.querySelector('.burger');
+// const menu = document.querySelector('.menu');
+// const closeElem = document.querySelector('.menu__close');
+
+// burger.addEventListener('click', () => {
+//   menu.classList.add('menu_active');
+// })
+
+// closeElem.addEventListener('click', () => {
+//   menu.classList.remove('menu_active');
+// })
+import Swiper from "swiper";
+
+
+const hotelSwiper = new Swiper(".hotel__slider", {
   loop: true,
-  speed: 500,
+  speed: 300,
   effect: 'fade',
   fade: {
     crossFade: true
@@ -18,5 +27,14 @@ const swiper = new Swiper(".hotel__slider", {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
+  },
+});
+
+const reviewsSwiper = new Swiper(".reviews__slider", {
+  loop: true,
+  speed: 300,
+  navigation: {
+    nextEl: ".reviews__slider-btn--next",
+    prevEl: ".reviews__slider-btn--prev",
   },
 });
