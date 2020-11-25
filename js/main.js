facfbba79574384d94ed6279d44025acddeda170
Slava Pogosov <pogoslav1@gmail.com>
2020-11-25 18:30:45 +0300

    create: js update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// const burger = document.querySelector('.burger');
-// const menu = document.querySelector('.menu');
-// const closeElem = document.querySelector('.menu__close');
-
-// burger.addEventListener('click', () => {
-//   menu.classList.add('menu_active');
-// })
-
-// closeElem.addEventListener('click', () => {
-//   menu.classList.remove('menu_active');
-// })
 import Swiper from "swiper";
 
 
