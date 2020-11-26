7b69fc0b05efa11985f5638400a0dc0aa58f4ea8
Slava Pogosov <pogoslav1@gmail.com>
2020-11-26 19:34:08 +0300

    create: update media

--- js/main.js
+++ js/main.js
@@ -1,40 +1,21 @@
-// const burger = document.querySelector('.burger');
-// const menu = document.querySelector('.menu');
-// const closeElem = document.querySelector('.menu__close');
-
-// burger.addEventListener('click', () => {
-//   menu.classList.add('menu_active');
-// })
-
-// closeElem.addEventListener('click', () => {
-//   menu.classList.remove('menu_active');
-// })
-import Swiper from "swiper";
-
-
-const hotelSwiper = new Swiper(".hotel__slider", {
-  loop: true,
-  speed: 300,
-  effect: 'fade',
-  fade: {
-    crossFade: true
-  },
-  navigation: {
-    nextEl: ".hotel__slider-btn--next",
-    prevEl: ".hotel__slider-btn--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-});
-
-const reviewsSwiper = new Swiper(".reviews__slider", {
-  loop: true,
-  speed: 300,
-  navigation: {
-    nextEl: ".reviews__slider-btn--next",
-    prevEl: ".reviews__slider-btn--prev",
-  },
-});
+var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev", 
+    },
+    effect: "coverflow",
+  });
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev", 
+    },
+  });
