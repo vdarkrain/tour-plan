9eb3aab6f0ef3509204558a11b055e322d67edc9
Slava Pogosov <pogoslav1@gmail.com>
2020-12-05 20:05:50 +0300

    create: update

---
+++ send.php
@@ -0,0 +1,74 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+$button = $_POST['button'];
+
+// Формирование самого письма
+if ($button == 'message-form'){
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+";
+};
+
+
+if ($button == 'subscribe-form'){
+  $title = "Новое подписка на рассылку Best Tour Plan";
+  $body = "
+  <h2>Новая подписка</h2>
+  <b>Email:</b> $email
+  ";
+};
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = ''; // Логин на почте
+    $mail->Password   = ''; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('pogoslav1@gmail.com', 'Slava Pogosov'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('wywkoff@gmail.com');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";}
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+if ($button == 'message-form'){
+  header('location: thanks.html');
+}
+
+if ($button == 'subscribe-form'){
+  header('location: subscribe.html');
+}
